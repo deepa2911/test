8eba70c8e3baf6be2215f54bb73b33fb1d2b870a
vijay <vijay@ADV_PC>
2022-12-10 23:09:41 -0800

    testjava

---
+++ src/main/java/testproject/testjava.java
@@ -0,0 +1,14 @@
+package testproject;
+
+public class testjava {
+
+	public testjava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("JAVA IS ");
+	}
+
+}
